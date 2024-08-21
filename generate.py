3b00ce149d58093243d820ed869bba8798637fd0
Alex Nelson <alexander.nelson@nist.gov>
2024-08-21 10:16:49 -0400

    Apply pre-commit effects and recommendations from Black, flake8, and isort
    Disclaimer:
    Participation by NIST in the creation of the documentation of mentioned
    software is not intended to imply a recommendation or endorsement by the
    National Institute of Standards and Technology, nor is it intended to
    imply that any specific software is necessarily the best available for
    the purpose.
    
    Signed-off-by: Alex Nelson <alexander.nelson@nist.gov>

--- generate.py
+++ generate.py
@@ -1,38 +1,53 @@
-import os
-import rdflib
-from copy import deepcopy
 import argparse
-import json
-from typing import Union
-from case_utils.namespace import *
-import case_utils.ontology
-from case_utils.ontology.version_info import CURRENT_CASE_VERSION
 import importlib.resources
+import json
+import os
+from typing import Union
 
+import case_utils.ontology
+import rdflib
+from case_utils.namespace import (
+    NS_CASE_INVESTIGATION,
+    NS_CASE_VOCABULARY,
+    NS_UCO_ACTION,
+    NS_UCO_CORE,
+    NS_UCO_IDENTITY,
+    NS_UCO_LOCATION,
+    NS_UCO_MARKING,
+    NS_UCO_OBSERVABLE,
+    NS_UCO_PATTERN,
+    NS_UCO_ROLE,
+    NS_UCO_TOOL,
+    NS_UCO_TYPES,
+    NS_UCO_VICTIM,
+    NS_UCO_VOCABULARY,
+)
+from case_utils.ontology.version_info import CURRENT_CASE_VERSION
 
-#NOTICE
+# NOTICE
 # This software was produced for the U.S. Government under contract FA8702-22-C-0001,
 # and is subject to the Rights in Data-General Clause 52.227-14, Alt. IV (DEC 2007)
 # ©2022 The MITRE Corporation. All Rights Reserved.
 # Released under PRS 18-4297.
 
 
-__version__ = '0.0.2'
+__version__ = "0.0.2"
 
 NS_SH = rdflib.SH
 NS_RDF = rdflib.RDF
 NS_XSD = rdflib.XSD
 
-caseutils_version  = case_utils.__version__
-#uco_version = '0.9.0' - don't know how uco denotes their version
+caseutils_version = case_utils.__version__
+# uco_version = '0.9.0' - don't know how uco denotes their version
 
-ignore_keys = ['http://www.w3.org/2000/01/rdf-schema#range', #rdf:range
-               'http://www.w3.org/2000/01/rdf-schema#label', #rdf:label
-               'http://www.w3.org/2000/01/rdf-schema#comment', #rdf:comment
-               'http://www.w3.org/ns/shacl#targetClass', #sh:targetClass
-              ]
+ignore_keys = [
+    "http://www.w3.org/2000/01/rdf-schema#range",  # rdf:range
+    "http://www.w3.org/2000/01/rdf-schema#label",  # rdf:label
+    "http://www.w3.org/2000/01/rdf-schema#comment",  # rdf:comment
+    "http://www.w3.org/ns/shacl#targetClass",  # sh:targetClass
+]
 
-#direct uco vocabulary
+# direct uco vocabulary
 # obs_prefix = {str(NS_UCO_OBSERVABLE):'observable:',
 #               str(NS_UCO_CORE):'core:',
 #               str(NS_UCO_TOOL):'tool:',
@@ -51,53 +66,62 @@ ignore_keys = ['http://www.w3.org/2000/01/rdf-schema#range', #rdf:range
 #               str(NS_UCO_TYPES):"types:",
 #              }
 
-#case-uco vocabulary
-obs_prefix = {  #uco vocabulary
-                str(NS_UCO_MARKING):'uco-marking:',
-                str(NS_UCO_TOOL):'uco-tool:',
-                str(NS_SH):'sh:',
-                str(NS_RDF):'rdfs:',
-                str(NS_XSD):'xsd:',
-                str(NS_RDF):'rdf:',
-                str(NS_UCO_OBSERVABLE):'uco-observable:',
-                str(NS_UCO_MARKING):'uco-marking:',
-                str(NS_UCO_IDENTITY):'uco-identity:',
-                str(NS_UCO_VICTIM):"uco-victim:",
-                str(NS_UCO_VOCABULARY):'uco-vocabulary:',
-                str(NS_UCO_PATTERN):'uco-pattern:',
-                str(NS_UCO_CORE):'uco-core:',
-                str(NS_UCO_TOOL):'uco-tool:',
-                str(NS_UCO_ACTION):'uco-action:',
-                str(NS_UCO_LOCATION):'uco-location:',
-                'http://www.w3.org/2000/01/rdf-schema#':'rdfs:',
-                str(NS_UCO_ROLE):"uco-role:",
-                str(NS_UCO_TYPES):"uco-types:",
-                #case vocab
-                str(NS_CASE_INVESTIGATION): "investigation:",
-                str(NS_CASE_VOCABULARY):'vocabulary:'}
+# case-uco vocabulary
+obs_prefix = {  # uco vocabulary
+    str(NS_UCO_MARKING): "uco-marking:",
+    str(NS_UCO_TOOL): "uco-tool:",
+    str(NS_SH): "sh:",
+    str(NS_RDF): "rdfs:",
+    str(NS_XSD): "xsd:",
+    str(NS_RDF): "rdf:",
+    str(NS_UCO_OBSERVABLE): "uco-observable:",
+    str(NS_UCO_MARKING): "uco-marking:",
+    str(NS_UCO_IDENTITY): "uco-identity:",
+    str(NS_UCO_VICTIM): "uco-victim:",
+    str(NS_UCO_VOCABULARY): "uco-vocabulary:",
+    str(NS_UCO_PATTERN): "uco-pattern:",
+    str(NS_UCO_CORE): "uco-core:",
+    str(NS_UCO_TOOL): "uco-tool:",
+    str(NS_UCO_ACTION): "uco-action:",
+    str(NS_UCO_LOCATION): "uco-location:",
+    "http://www.w3.org/2000/01/rdf-schema#": "rdfs:",
+    str(NS_UCO_ROLE): "uco-role:",
+    str(NS_UCO_TYPES): "uco-types:",
+    # case vocab
+    str(NS_CASE_INVESTIGATION): "investigation:",
+    str(NS_CASE_VOCABULARY): "vocabulary:",
+}
 
 reverse_obs_prefix = {}
-for k,v in obs_prefix.items():
+for k, v in obs_prefix.items():
     reverse_obs_prefix[v] = k
 
-def reducestring(string:str, returnChange=False):
-    for k,v in obs_prefix.items():
+
+def reducestring(string: str, returnChange=False):
+    for k, v in obs_prefix.items():
         if k in string:
             if returnChange:
-                return string.replace(k,v),True
+                return string.replace(k, v), True
             else:
-                return string.replace(k,v)
+                return string.replace(k, v)
     if returnChange:
-        return str(string),False
+        return str(string), False
     else:
         return str(string)
 
 
 def makedirs(directory):
-    os.makedirs(f'{directory}',exist_ok = True)
+    os.makedirs(f"{directory}", exist_ok=True)
+
 
 class main:
-    def __init__(self,ontology_dir:str = None,directory:str = "templates", useCaseUtils:bool = False, short:bool = False ):
+    def __init__(
+        self,
+        ontology_dir: str = None,
+        directory: str = "templates",
+        useCaseUtils: bool = False,
+        short: bool = False,
+    ):
         makedirs(directory)
 
         self.switch = useCaseUtils
@@ -115,26 +139,27 @@ class main:
             for onto in ontology_dir:
                 if os.path.isdir(onto):
                     for onto in self.onto_dir:
-                        for root, dirs, files in os.walk(onto, topdown = False):
-                           for name in files:
-                               if name.endswith(".ttl"):
-                                  adir = os.path.join(root, name)
-                                  if adir not in self.files_dir:
-                                      self.files_dir.append(adir)
+                        for root, dirs, files in os.walk(onto, topdown=False):
+                            for name in files:
+                                if name.endswith(".ttl"):
+                                    adir = os.path.join(root, name)
+                                    if adir not in self.files_dir:
+                                        self.files_dir.append(adir)
                 elif os.path.isfile(onto):
-                  if adir not in self.files_dir:
-                      self.files_dir.append(adir)
+                    if adir not in self.files_dir:
+                        self.files_dir.append(adir)
         self.directory = directory
 
-    def paduco(self,string):
-        v = string.split(":")[0]+":"
+    def paduco(self, string):
+        v = string.split(":")[0] + ":"
         if v in reverse_obs_prefix:
-            return string #don't pad eg. investigation
+            return string  # don't pad eg. investigation
         else:
-            return self.prepad+"-"+string #if not defined, pad it.
-    def removepad(self,string):
+            return self.prepad + "-" + string  # if not defined, pad it.
+
+    def removepad(self, string):
         if string.startswith(self.prepad):
-            return string.replace(self.prepad + "-","")
+            return string.replace(self.prepad + "-", "")
         else:
             return string
 
@@ -144,48 +169,71 @@ class main:
             for file in self.files_dir:
                 self.g.parse(file)
             if self.switch:
-                case_utils.ontology.load_subclass_hierarchy(g)
+                case_utils.ontology.load_subclass_hierarchy(self.g)
         else:
-            ttl_filename = "case-"+CURRENT_CASE_VERSION+".ttl"
+            ttl_filename = "case-" + CURRENT_CASE_VERSION + ".ttl"
             ttl_data = importlib.resources.read_text(case_utils.ontology, ttl_filename)
             self.g.parse(data=ttl_data)
             case_utils.ontology.load_subclass_hierarchy(self.g)
 
     def load_case_version(self):
         try:
-            self.case_version = str([list(i) for i in self.g.query('SELECT ?s ?p ?o WHERE{?s owl:versionInfo ?o}')][0][2])
-        except:
+            self.case_version = str(
+                [
+                    list(i)
+                    for i in self.g.query(
+                        "SELECT ?s ?p ?o WHERE{?s owl:versionInfo ?o}"
+                    )
+                ][0][2]
+            )
+        except Exception:
             self.case_version = CURRENT_CASE_VERSION
 
-
-    def getSubClassOf(self,name:str):
-        res = [i for i in self.g.query("SELECT ?o WHERE {{ {} rdfs:subClassOf ?o }}".format(self.removepad(name)))]
+    def getSubClassOf(self, name: str):
+        res = [
+            i
+            for i in self.g.query(
+                "SELECT ?o WHERE {{ {} rdfs:subClassOf ?o }}".format(
+                    self.removepad(name)
+                )
+            )
+        ]
         if res:
             return res[0]
-        return res #comes back as list of triples
+        return res  # comes back as list of triples
 
-    def getProperty(self,name:str):
-        res = [i for i in self.g.query("SELECT ?o WHERE {{ {} sh:property ?o }}".format(self.removepad(name)))]
-        return res #comes back as list of triples
+    def getProperty(self, name: str):
+        res = [
+            i
+            for i in self.g.query(
+                "SELECT ?o WHERE {{ {} sh:property ?o }}".format(self.removepad(name))
+            )
+        ]
+        return res  # comes back as list of triples
 
-    def getPath(self,name:str):
-        res = [i for i in self.g.query("SELECT ?o WHERE {{ {} sh:path ?o }}".format(self.removepad(name)))]
-        return res #comes back as list of triples
+    def getPath(self, name: str):
+        res = [
+            i
+            for i in self.g.query(
+                "SELECT ?o WHERE {{ {} sh:path ?o }}".format(self.removepad(name))
+            )
+        ]
+        return res  # comes back as list of triples
 
-    def getUCOname(self,name):
+    def getUCOname(self, name):
         return self.removepad(reducestring(name))
 
-    def getParents(self,name:str,hist: list = [] ):
+    def getParents(self, name: str, hist: list = []):
         parents = self.getSubClassOf(name)
         if not parents:
             return []
         else:
-            for parent in parents: #DFS
+            for parent in parents:  # DFS
                 p = self.getUCOname(parent)
                 if p not in hist:
                     hist.append(p)
-                #check if each parent has parents that isn't part of hist
-                for i in self.getParents(p,hist):
+                # check if each parent has parents that isn't part of hist
+                for i in self.getParents(p, hist):
                     if i not in hist:
                         hist.append(i)
         return hist
@@ -209,48 +257,47 @@ class main:
             s, p = triple
             self.class_names.append(self.removepad(reducestring(s)))
 
-    def findContext(self,dict_graph:dict):
+    def findContext(self, dict_graph: dict):
         c = {}
         for t in dict_graph.keys():
-            for k,v in obs_prefix.items():
+            for k, v in obs_prefix.items():
                 if self.paduco(t).startswith(v):
                     c[self.paduco(v.strip(":"))] = k.strip("/") + "#"
         return c
 
-    def load_single(self,name:str):
-        single = {'@context':{},
-                  '@graph':[{}]
-                 }
-        single['@context']['kb'] = 'http://example.org/kb/'
+    def load_single(self, name: str):
+        single = {"@context": {}, "@graph": [{}]}
+        single["@context"]["kb"] = "http://example.org/kb/"
         n = "".join(name.split(":")[1:])
-        single['@graph'][0]['@id'] = "kb:"+n.lower()+"1"
-        single['@graph'][0]['@type'] = self.paduco(name)
+        single["@graph"][0]["@id"] = "kb:" + n.lower() + "1"
+        single["@graph"][0]["@type"] = self.paduco(name)
 
-        #add the properties of the object
+        # add the properties of the object
         props = self.getProperty(name)
         for prop in props:
             for p in self.bnode_dict[str(prop[0])]:
                 if p:
-                    single['@graph'][0][self.paduco(p)] = None
+                    single["@graph"][0][self.paduco(p)] = None
 
-        single['@context'].update(self.findContext({name:None}))
-        single['@context'].update(self.findContext(single['@graph'][0]))
-        single['@version']={'case_util':caseutils_version,'ontology_version':self.case_version}
-
+        single["@context"].update(self.findContext({name: None}))
+        single["@context"].update(self.findContext(single["@graph"][0]))
+        single["@version"] = {
+            "case_util": caseutils_version,
+            "ontology_version": self.case_version,
+        }
 
         if self.generate_short:
             pass
         else:
-            #add the parent's properties
-            parents = self.getParents(name,[])
+            # add the parent's properties
+            parents = self.getParents(name, [])
             for parent in parents:
                 props = self.getProperty(parent)
                 for prop in props:
                     for node in self.bnode_dict[str(prop[0])]:
-                        single['@graph'][0][self.paduco(node)] = None
-            single['@context'].update(self.findContext(single['@graph'][0]))
+                        single["@graph"][0][self.paduco(node)] = None
+            single["@context"].update(self.findContext(single["@graph"][0]))
         return single
-
 
     def generate(self):
         self.load_graph()
@@ -260,8 +307,7 @@ class main:
         self.generate_classes()
         return
 
-
-    def convertToJson(self,obj_name:str):
+    def convertToJson(self, obj_name: str):
         vocab, newname = obj_name.split(":")
         obj = self.load_single(obj_name)
 
@@ -271,12 +317,12 @@ class main:
         nextdir = f"{self.directory}/{self.paduco(vocab)}"
         makedirs(nextdir)
 
-        with open(f"{nextdir}/{newname}.json", 'w') as fl:
-            json.dump(obj, fl,indent=2)
+        with open(f"{nextdir}/{newname}.json", "w") as fl:
+            json.dump(obj, fl, indent=2)
             fl.close()
         print(f"Success:{self.paduco(obj_name)}")
 
-    def run(self,name:Union[list,str] = None):
+    def run(self, name: Union[list, str] = None):
         if not name:
             name = self.class_names
         else:
@@ -285,18 +331,42 @@ class main:
             self.convertToJson(k)
         return
 
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument('-o',"--ontology", help="ontology ttl file.",type=str,required=False)
-    parser.add_argument("--output", help="default output folder for studs",type=str,required=False,default = "templates")
+    parser.add_argument(
+        "-o", "--ontology", help="ontology ttl file.", type=str, required=False
+    )
+    parser.add_argument(
+        "--output",
+        help="default output folder for studs",
+        type=str,
+        required=False,
+        default="templates",
+    )
 
-    parser.add_argument("-s", "--specific", help="specific single object name",type=str,required=False)
-    parser.add_argument("-a", "--caseutil", help="[T/F] allow case_utils to load uco ontology.",type=bool,required=False,default=False)
-    parser.add_argument("-t", "--short", help="[T/F] generate short stub (no superclass properties) or full stub.",type=bool,required=False,default=False)
+    parser.add_argument(
+        "-s", "--specific", help="specific single object name", type=str, required=False
+    )
+    parser.add_argument(
+        "-a",
+        "--caseutil",
+        help="[T/F] allow case_utils to load uco ontology.",
+        type=bool,
+        required=False,
+        default=False,
+    )
+    parser.add_argument(
+        "-t",
+        "--short",
+        help="[T/F] generate short stub (no superclass properties) or full stub.",
+        type=bool,
+        required=False,
+        default=False,
+    )
     args = parser.parse_args()
 
-    obj = main(args.ontology,args.output,args.caseutil,args.short)
-
+    obj = main(args.ontology, args.output, args.caseutil, args.short)
 
     makedirs(args.output)
     obj.generate()
